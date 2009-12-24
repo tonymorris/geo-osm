36cab780d92f261067508ebb6cd446f86fd4708a
Tony Morris <tmorris@tmorris.net>
2009-12-24 13:21:41 +1000

    Added a Util function in preparation for splitting

--- Geo/OSM/Util.hs
+++ Geo/OSM/Util.hs
@@ -1,6 +1,7 @@
 -- | Utilities for parsing OSM files.
 module Geo.OSM.Util(
-                     showPickled
+                     showPickled,
+                     textAttr
                    ) where
 
 import Text.XML.HXT.Arrow
@@ -8,3 +9,6 @@ import Text.XML.HXT.Arrow
 -- | Pickles a value then writes the document to a string.
 showPickled :: (XmlPickler a) => Attributes -> a -> String
 showPickled a = concat . (pickleDoc xpickle >>> runLA (writeDocumentToString a))
+
+textAttr :: String -> PU String
+textAttr = flip xpAttr xpText
