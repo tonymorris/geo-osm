f976fcf0f3938566c2a9d4e6e53572381662d1fb
Tony Morris <tmorris@tmorris.net>
2010-01-03 12:06:18 +1000

    Interact functions

--- Data/Geo/OSM/OSM.hs
+++ Data/Geo/OSM/OSM.hs
@@ -4,11 +4,22 @@
 module Data.Geo.OSM.OSM(
                     OSM,
                     osm,
-                    readOsmFile
+                    readOsmFile,
+                    readOsmFiles,
+                    interactOSMIO,
+                    interactsOSMIO,
+                    interactOSMIO',
+                    interactsOSMIO',
+                    interactOSM,
+                    interactsOSM,
+                    interactOSM',
+                    interactsOSM'
                   ) where
 
 import Text.XML.HXT.Arrow
 import Text.XML.HXT.Extras
+import Control.Monad
+import Data.List
 import Data.Geo.OSM.OSMChildren
 import Data.Geo.OSM.Bound
 import Data.Geo.OSM.Bounds
@@ -62,3 +73,78 @@ osm = OSM
 readOsmFile :: FilePath -> IO [OSM]
 readOsmFile = runX . xunpickleDocument (xpickle :: PU OSM) [(a_remove_whitespace, v_1)]
 
+-- | Reads 0 or more OSM files into a list of @OSM@ values removing whitespace.
+readOsmFiles :: [FilePath] -> IO [OSM]
+readOsmFiles = fmap join . (mapM readOsmFile)
+
+-- | Reads a OSM file, executes the given function on the XML, then writes the given file.
+interactOSMIO' :: Attributes -- ^ The options for reading the OSM file.
+                  -> FilePath -- ^ The OSM file to read.
+                  -> (OSM -> IO OSM) -- ^ The function to execute on the XML that is read.
+                  -> Attributes -- ^ The options for writing the OSM file.
+                  -> FilePath -- ^ The OSM file to write.
+                  -> IO ()
+interactOSMIO' froma from f toa to = runX (xunpickleDocument (xpickle :: PU OSM) froma from >>> arrIO f >>> xpickleDocument (xpickle :: PU OSM) toa to) >> return ()
+
+-- | Reads a OSM file, executes the given functions on the XML, then writes the given file.
+interactsOSMIO' :: Attributes -- ^ The options for reading the OSM file.
+                   -> FilePath -- ^ The OSM file to read.
+                   -> [OSM -> IO OSM] -- ^ The function to execute on the XML that is read.
+                   -> Attributes -- ^ The options for writing the OSM file.
+                   -> FilePath -- ^ The OSM file to write.
+                   -> IO ()
+interactsOSMIO' froma from = interactOSMIO' froma from . sumIO'
+
+-- | Reads a OSM file removing whitespace, executes the given function on the XML, then writes the given file with indentation.
+interactOSMIO :: FilePath -- ^ The OSM file to read.
+                 -> (OSM -> IO OSM) -- ^ The function to execute on the XML that is read.
+                 -> FilePath -- ^ The OSM file to write.
+                 -> IO ()
+interactOSMIO from f = interactOSMIO' [(a_remove_whitespace, v_1)] from f [(a_indent, v_1)]
+
+-- | Reads a OSM file removing whitespace, executes the given functions on the XML, then writes the given file with indentation.
+interactsOSMIO :: FilePath -- ^ The OSM file to read.
+                  -> [OSM -> IO OSM] -- ^ The function to execute on the XML that is read.
+                  -> FilePath -- ^ The OSM file to write.
+                  -> IO ()
+interactsOSMIO from = interactOSMIO from . sumIO'
+
+-- | Reads a OSM file, executes the given function on the XML, then writes the given file.
+interactOSM' :: Attributes -- ^ The options for reading the OSM file.
+                -> FilePath -- ^ The OSM file to read.
+                -> (OSM -> OSM) -- ^ The function to execute on the XML that is read.
+                -> Attributes -- ^ The options for writing the OSM file.
+                -> FilePath -- ^ The OSM file to write.
+                -> IO ()
+interactOSM' froma from f = interactOSMIO' froma from (return . f)
+
+-- | Reads a OSM file, executes the given functions on the XML, then writes the given file.
+interactsOSM' :: Attributes -- ^ The options for reading the OSM file.
+                 -> FilePath -- ^ The OSM file to read.
+                 -> [OSM -> OSM] -- ^ The functions to execute on the XML that is read.
+                 -> Attributes -- ^ The options for writing the OSM file.
+                 -> FilePath -- ^ The OSM file to write.
+                 -> IO ()
+interactsOSM' froma from = interactOSM' froma from . sum'
+
+-- | Reads a OSM file removing whitespace, executes the given function on the XML, then writes the given file with indentation.
+interactOSM :: FilePath -- ^ The OSM file to read.
+               -> (OSM -> OSM) -- ^ The function to execute on the XML that is read.
+               -> FilePath -- ^ The OSM file to write.
+               -> IO ()
+interactOSM from f = interactOSMIO from (return . f)
+
+-- | Reads a OSM file removing whitespace, executes the given functions on the XML, then writes the given file with indentation.
+interactsOSM :: FilePath -- ^ The OSM file to read.
+                -> [OSM -> OSM] -- ^ The function to execute on the XML that is read.
+                -> FilePath -- ^ The OSM file to write.
+                -> IO ()
+interactsOSM from = interactOSM from . sum'
+
+-- not exported
+
+sum' :: [a -> a] -> a -> a
+sum' = foldl' (.) id
+
+sumIO' :: (Monad m) => [a -> m a] -> a -> m a
+sumIO' x = foldl' (>=>) return x
