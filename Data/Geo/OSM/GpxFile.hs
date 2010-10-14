758afcc7f0a89871ae6eedcce2bfef0d5d33ef13
Tony Morris <tmorris@tmorris.net>
2010-10-14 15:26:47 +1000

    Tidy

--- Data/Geo/OSM/GpxFile.hs
+++ Data/Geo/OSM/GpxFile.hs
@@ -37,9 +37,10 @@ gpxFile =
   GpxFile
 
 instance XmlPickler GpxFile where
-  xpickle = let b = xpWrapMaybe (\s -> case fmap toLower s of "true" -> Just True
-                                                              "false" -> Just False
-                                                              _ -> Nothing, fmap toLower . show)
+  xpickle = let b = xpWrapMaybe (\s -> case fmap toLower s
+                                       of "true"  -> Just True
+                                          "false" -> Just False
+                                          _       -> Nothing, fmap toLower . show)
             in xpElem "gpx_file" (xpWrap (\(id'', name', lat', lon', user', public', pending', timestamp') -> gpxFile id'' name' lat' lon' user' public' pending' timestamp', \(GpxFile id'' name' lat' lon' user' public' pending' timestamp') -> (id'', name', lat', lon', user', public', pending', timestamp')) (xp8Tuple (xpAttr "id" xpText) (xpAttr "name" xpText) (xpAttr "lat" xpText) (xpAttr "lon" xpText) (xpAttr "user" xpText) (xpDefault False (b (xpAttr "public" xpText))) (xpDefault False (b (xpAttr "pending" xpText))) (xpAttr "timestamp" xpText)))
 
 instance Show GpxFile where
