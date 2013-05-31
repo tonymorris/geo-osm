0322333820d5961640c587f8829213d3d64f5085
Tony Morris <tmorris@tmorris.net>
2013-06-01 09:10:01 +1000

    module layout

--- src/Data/Geo/OSM.hs
+++ src/Data/Geo/OSM.hs
@@ -1,51 +1,26 @@
 -- | OpenStreetMap API v0.6 DTD <http://wiki.openstreetmap.org/wiki/API_v0.6/DTD>
-module Data.Geo.OSM
-(
-  module Data.Geo.OSM.Api
-, module Data.Geo.OSM.Area
-, module Data.Geo.OSM.Bound
-, module Data.Geo.OSM.Bounds
-, module Data.Geo.OSM.Changeset
-, module Data.Geo.OSM.GpxFile
-, module Data.Geo.OSM.Home
-, module Data.Geo.OSM.Member
-, module Data.Geo.OSM.MemberType
-, module Data.Geo.OSM.Nd
-, module Data.Geo.OSM.Node
-, module Data.Geo.OSM.NodeWayRelation
-, module Data.Geo.OSM.OSM
-, module Data.Geo.OSM.Children
-, module Data.Geo.OSM.Preferences
-, module Data.Geo.OSM.Relation
-, module Data.Geo.OSM.Tag
-, module Data.Geo.OSM.Tracepoints
-, module Data.Geo.OSM.User
-, module Data.Geo.OSM.Version
-, module Data.Geo.OSM.Way
-, module Data.Geo.OSM.Waynodes
-, module Data.Geo.OSM.Lens
-) where
+module Data.Geo.OSM(module O) where
 
-import Data.Geo.OSM.Api
-import Data.Geo.OSM.Area
-import Data.Geo.OSM.Bound
-import Data.Geo.OSM.Bounds
-import Data.Geo.OSM.Changeset
-import Data.Geo.OSM.GpxFile
-import Data.Geo.OSM.Home
-import Data.Geo.OSM.Member
-import Data.Geo.OSM.MemberType
-import Data.Geo.OSM.Nd
-import Data.Geo.OSM.Node
-import Data.Geo.OSM.NodeWayRelation
-import Data.Geo.OSM.OSM
-import Data.Geo.OSM.Children
-import Data.Geo.OSM.Preferences
-import Data.Geo.OSM.Relation
-import Data.Geo.OSM.Tag
-import Data.Geo.OSM.Tracepoints
-import Data.Geo.OSM.User
-import Data.Geo.OSM.Version
-import Data.Geo.OSM.Way
-import Data.Geo.OSM.Waynodes
-import Data.Geo.OSM.Lens
+import Data.Geo.OSM.Api as O
+import Data.Geo.OSM.Area as O
+import Data.Geo.OSM.Bound as O
+import Data.Geo.OSM.Bounds as O
+import Data.Geo.OSM.Changeset as O
+import Data.Geo.OSM.GpxFile as O
+import Data.Geo.OSM.Home as O
+import Data.Geo.OSM.Member as O
+import Data.Geo.OSM.MemberType as O
+import Data.Geo.OSM.Nd as O
+import Data.Geo.OSM.Node as O
+import Data.Geo.OSM.NodeWayRelation as O
+import Data.Geo.OSM.OSM as O
+import Data.Geo.OSM.Children as O
+import Data.Geo.OSM.Preferences as O
+import Data.Geo.OSM.Relation as O
+import Data.Geo.OSM.Tag as O
+import Data.Geo.OSM.Tracepoints as O
+import Data.Geo.OSM.User as O
+import Data.Geo.OSM.Version as O
+import Data.Geo.OSM.Way as O
+import Data.Geo.OSM.Waynodes as O
+import Data.Geo.OSM.Lens as O
